c834c2d59e74e804429b6467b0c408f859a06f2a
Fredrik Aschehoug <fredrik.aschehoug@norclub.com>
2019-03-06 15:20:56 +0100

    Added error handling to Nim constructor

--- js/nim.js
+++ js/nim.js
@@ -1,5 +1,19 @@
 //Nim constructor
 function Nim(player1, player2, victory, total, maxGrab){
+    // Error handling
+    if(maxGrab < 2 || maxGrab > 4) {
+        throw "‘maxGrab’ must be between 2 and 4";
+    }
+    if(player1.name == undefined || player1.human == undefined){
+        throw "'player1' is missing a value in name or human";
+    }
+    if(player2.name == undefined || player2.human == undefined){
+        throw "'player2' is missing a value in name or human";
+    }
+    if(total < 12){
+        throw "'total' must be at least 12";
+    }
+
     this.player1 = player1;
     this.player2 = player2;
     this.victory = victory;
@@ -7,7 +21,7 @@ function Nim(player1, player2, victory, total, maxGrab){
     this.maxGrab = maxGrab || 3;
     this.makeMove = function(grabAmount){
         this.total -= grabAmount;
-    }
+    }  
 }
 
-
+// Oppgave 2.1 og 2.2 er løst her
