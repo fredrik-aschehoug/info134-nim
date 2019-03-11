56c14963ff5f1f098801240a032a8b0379a4c58a
Fredrik Aschehoug <fredrik.aschehoug@norclub.com>
2019-03-11 10:02:20 +0100

    Implemented AI

--- js/game.js
+++ js/game.js
@@ -13,6 +13,15 @@ let player2Button1;
 let player2Button2;
 let player2Button3;
 
+function getAIAmount(){
+    let amount;
+    if(nimObj.total < 4){
+        amount = nimObj.total;
+    } else {
+        amount = Math.floor((Math.random() * 3) + 1);
+    }
+    return amount;
+}
 
 //Button click functions
 function buttonClickPlayer1(amount){
@@ -22,6 +31,10 @@ function buttonClickPlayer1(amount){
         nimObj.reverseTokens();
         setCurrentPlayer(nimObj.player2.name);
         disableInvalidButtons(nimObj.total);
+        if(!nimObj.player2.human){
+            let amount = getAIAmount();
+            buttonClickPlayer2(amount);
+        }
     }
 }
 
@@ -80,6 +93,13 @@ function initGame(nimObj){
     player2Button3.onclick = () => buttonClickPlayer2(3);
 
     setCurrentPlayer(nimObj.player1.name);
+    
+    // Disable buttons if AI
+    if(!nimObj.player2.human){
+        player2Button1.disabled = true;
+        player2Button2.disabled = true;
+        player2Button3.disabled = true;
+    }
 }
 
 
@@ -88,17 +108,35 @@ const playerNamesForm = document.getElementById("playerNamesForm");
 let startGame = document.getElementById("startGame"); 
 startGame.onclick = () => {
     const player1Name = playerNamesForm.elements["player1"].value;
-    const player2Name = playerNamesForm.elements["player2"].value;
-    
+    const player2Name = playerNamesForm.elements["player2"].value; 
+    let player2;   
+    if(player1Name != "" && player2Name != "") {
+        if (player2Name === "AI"){
+            player2 = {
+                name: "AI",
+                human: false
+            }
+        } else {
+            player2 = {
+                name: player2Name,
+                human: true
+            }
+        }
+    } else if (player1Name != "" && player2Name === "") {
+        playerNamesForm.elements["player2"].value = "AI";
+        player2 = {
+            name: "AI",
+            human: false
+        }
+    } else {
+        alert("Player 1 needs a name!")
+        throw "Player 1 is missing name"
+    }
     const player1 = {
         name: player1Name,
         human: true
     }
     
-    const player2 = {
-        name: player2Name,
-        human: true
-    }
     // Create game object
     nimObj = new Nim(player1, player2, victory, total);
     
