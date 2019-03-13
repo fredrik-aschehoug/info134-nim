be93e2801b10b05384bb46795fa96e091dcc68f5
fredrik-aschehoug <15358786+fredrik-aschehoug@users.noreply.github.com>
2019-03-13 09:31:31 +0100

    Refactoring

--- js/game.js
+++ js/game.js
@@ -151,36 +151,7 @@ function initGame(nimObj){
     }
 }
 
-
-function startGamePrompt() {
-    this.render = function(dialog, func){
-        let winW = window.innerWidth;
-        let winH = window.innerHeight; //to center window
-        let promptOverlay = document.getElementById('promptOverlay');
-        let promptBox = document.getElementById('promptBox');
-        promptOverlay.style.display = "block";
-        promptOverlay.style.height = winH + "em";
-
-        
-        promptBox.style.display = "block";
-        document.getElementById('promptBoxHead').innerHTML = "Setup"
-        document.getElementById('promptBoxBody').innerHTML = dialog;
-        document.getElementById('promptBoxBody').innerHTML += '<input id="promptValue1" type="text" name="player1" placeholder="Player 1 name">';
-        document.getElementById('promptBoxBody').innerHTML += '<input id="promptValue2" type="text" name="player2" placeholder="Player 2 name"><br>';
-        document.getElementById('promptBoxBody').innerHTML += '<input id="radioThree" type="radio" name="btnNum" value="3"> 3 &emsp;';
-        document.getElementById('promptBoxBody').innerHTML += '<input id="radioFour" type="radio" name="btnNum" value="4"> 4';
-        document.getElementById('promptBoxFoot').innerHTML = '<button id="startGame" onclick="Prompt.ok(\''+func+'\')">Start game</button>';
-    }
-   this.ok = function(func){
-        window["startDatGame"](promptValue1, promptValue2);
-        document.getElementById('promptBox').style.display = "none";
-        document.getElementById('promptOverlay').style.display = "none";
-    }
-}
-
-let Prompt = new startGamePrompt();
-
-
+let Prompt = new StartGamePrompt();
 
 //Configure 'Start Game' button
 const promptBoxBody = document.getElementById("promptBoxBody");
